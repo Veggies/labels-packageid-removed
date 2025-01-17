519cfd70bba068092050f611553b6c01501f6c05
Veggies <xboxcheeb.veggies@gmail.com>
2025-01-17 11:31:27 -0700

    Update nordvpn.sh
    Omitted packageID

--- fragments/labels/nordvpn.sh
+++ fragments/labels/nordvpn.sh
@@ -1,7 +1,7 @@
 nordvpn)
     name="NordVPN"
     type="pkg"
-    packageID="com.nordvpn.macos"
+    #packageID="com.nordvpn.macos"
     downloadURL="https://downloads.nordcdn.com/apps/macos/generic/NordVPN-OpenVPN/latest/NordVPN.pkg"
     appNewVersion=$( curl -s https://downloads.nordcdn.com/apps/macos/generic/NordVPN-OpenVPN/latest/update_pkg.xml | xpath '(//sparkle:shortVersionString/text())[1]' 2>/dev/null )
     versionKey="CFBundleShortVersionString"
