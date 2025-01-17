65b94a3258f218dfcd1f50d386e9c423d5b05643
Veggies <xboxcheeb.veggies@gmail.com>
2025-01-17 11:15:19 -0700

    Update adobeacrobatprodc.sh
    packageID removed

--- fragments/labels/adobeacrobatprodc.sh
+++ fragments/labels/adobeacrobatprodc.sh
@@ -3,7 +3,7 @@ adobeacrobatprodc)
     appName="Acrobat Distiller.app"
     type="pkgInDmg"
     pkgName="Acrobat/Acrobat DC Installer.pkg"
-    packageID="com.adobe.acrobat.DC.viewer.app.pkg.MUI"
+    packageID=""
     downloadURL="https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/osx10/Acrobat_DC_Web_WWMUI.dmg"
     expectedTeamID="JQ525L2MZD"
     blockingProcesses=( "Acrobat Pro DC" )
