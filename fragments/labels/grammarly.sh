536cc93c76712bbe57d56b4675d7482d560f893a
Veggies <xboxcheeb.veggies@gmail.com>
2025-01-17 11:23:12 -0700

    Update grammarly.sh
    Disable packageID

--- fragments/labels/grammarly.sh
+++ fragments/labels/grammarly.sh
@@ -1,7 +1,7 @@
 grammarly)
      name="Grammarly Desktop"
      type="dmg"
-     packageID="com.grammarly.ProjectLlama"
+     #packageID="com.grammarly.ProjectLlama"
      downloadURL="https://download-mac.grammarly.com/Grammarly.dmg"
      expectedTeamID="W8F64X92K3"
      # appName="Grammarly Installer.app"
